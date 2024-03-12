31a8dd0359ea7050dca4907a61f990b82ef504e8
wasituf <wasit.allin1@gmainl.com>
2024-03-12 22:36:15 +0600

    Updated nvim package config: glow
    Upated configuration options for glow.nvim to get rounded corners and
    larger window.

--- .config/nvim/lua/wasituf/plugins/glow.lua
+++ .config/nvim/lua/wasituf/plugins/glow.lua
@@ -1,6 +1,15 @@
 return {
   "ellisonleao/glow.nvim", 
   lazy = true,
-  config = true, 
+  config = function()
+    require("glow").setup({
+      border = "rounded",
+      style = "dark",
+      width = 100,
+      height = 200,
+      width_ratio = 0.8,
+      height_ratio = 0.8
+    })
+  end, 
   cmd = "Glow"
 }
