c77222dfb821add105223653635304c6d28e19e8
wasituf <wasit.allin1@gmainl.com>
2024-03-25 05:22:00 +0600

    Changed nvim-tree settings
    Changed nvim-tree location from right to floating window in the center.
    Changed nvim-tree arrow icons.

--- .config/nvim/lua/wasituf/plugins/nvim-tree.lua
+++ .config/nvim/lua/wasituf/plugins/nvim-tree.lua
@@ -1,5 +1,3 @@
-
-
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -116,18 +114,44 @@ return {
       vim.keymap.set("n", "ga", git_add, opts("Git add"))
     end
 
+    local HEIGHT_RATIO = 0.8
+    local WIDTH_RATIO = 0.65
+
     nvimtree.setup({
       on_attach = my_on_attach,
       view = {
-        side = 'right',
-        width = 34,
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2)
+            - vim.opt.cmdheight:get()
+            return {
+              border = 'rounded',
+              relative = 'editor',
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+        end,
       },
       renderer = {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = "",
-              arrow_open = "",
+              arrow_closed = "",
+              arrow_open = "",
             },
           },
         },
