0920bdd7109f044d6ff894cdcf6ee37f012462af
wasituf <wasit.allin1@gmainl.com>
2024-03-26 18:35:55 +0600

    Added plugins: neogit, nvim-lsp-file-operations
    Added neogit for git operations inside neovim. Added
    nvim-lsp-file-operations for auto changing import directories in files.

---
+++ .config/nvim/lua/wasituf/plugins/neogit.lua
@@ -0,0 +1,108 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = function()
+    local neogit = require("neogit")
+
+    neogit.setup {
+      graph_style = "unicode",
+      use_default_keymaps = true,
+      kind = "tab",
+      signs = {
+        -- { CLOSED, OPENED }
+        hunk = { "", "" },
+        item = { "", "" },
+        section = { "", "" },
+      },
+      integrations = {
+        telescope = nil,
+        diffview = nil,
+      },
+      mappings = {
+        commit_editor = {
+          ["q"] = "Close",
+          ["<leader>w"] = "Submit",
+          ["<leader>q"] = "Abort",
+        },
+        rebase_editor = {
+          ["p"] = "Pick",
+          ["r"] = "Reword",
+          ["e"] = "Edit",
+          ["s"] = "Squash",
+          ["f"] = "Fixup",
+          ["x"] = "Execute",
+          ["d"] = "Drop",
+          ["b"] = "Break",
+          ["q"] = "Close",
+          ["<cr>"] = "OpenCommit",
+          ["gk"] = "MoveUp",
+          ["gj"] = "MoveDown",
+          ["<c-c><c-c>"] = "Submit",
+          ["<c-c><c-k>"] = "Abort",
+        },
+        finder = {
+          ["<cr>"] = "Select",
+          ["<c-c>"] = "Close",
+          ["<esc>"] = "Close",
+          ["<c-n>"] = "Next",
+          ["<c-p>"] = "Previous",
+          ["<down>"] = "Next",
+          ["<up>"] = "Previous",
+          ["<tab>"] = "MultiselectToggleNext",
+          ["<s-tab>"] = "MultiselectTogglePrevious",
+          ["<c-j>"] = "NOP",
+        },
+        -- Setting any of these to `false` will disable the mapping.
+        popup = {
+          ["?"] = "HelpPopup",
+          ["A"] = "CherryPickPopup",
+          ["D"] = "DiffPopup",
+          ["M"] = "RemotePopup",
+          ["P"] = "PushPopup",
+          ["X"] = "ResetPopup",
+          ["Z"] = "StashPopup",
+          ["b"] = "BranchPopup",
+          ["c"] = "CommitPopup",
+          ["f"] = "FetchPopup",
+          ["l"] = "LogPopup",
+          ["m"] = "MergePopup",
+          ["p"] = "PullPopup",
+          ["r"] = "RebasePopup",
+          ["v"] = "RevertPopup",
+          ["w"] = "WorktreePopup",
+        },
+        status = {
+          ["q"] = "Close",
+          ["I"] = "InitRepo",
+          ["1"] = "Depth1",
+          ["2"] = "Depth2",
+          ["3"] = "Depth3",
+          ["4"] = "Depth4",
+          ["<tab>"] = "Toggle",
+          ["x"] = "Discard",
+          ["s"] = "Stage",
+          ["S"] = "StageUnstaged",
+          ["<c-s>"] = "StageAll",
+          ["u"] = "Unstage",
+          ["U"] = "UnstageStaged",
+          ["$"] = "CommandHistory",
+          ["#"] = "Console",
+          ["Y"] = "YankSelected",
+          ["<c-r>"] = "RefreshBuffer",
+          ["<enter>"] = "GoToFile",
+          ["<c-v>"] = "VSplitOpen",
+          ["<c-x>"] = "SplitOpen",
+          ["<c-t>"] = "TabOpen",
+          ["{"] = "GoToPreviousHunkHeader",
+          ["}"] = "GoToNextHunkHeader",
+        },
+      },
+    }
+  end
+}
+
