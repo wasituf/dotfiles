13b5e76244b7f59f11f1d95453ebd335b300b579
wasituf <wasit.allin1@proton.me>
2025-04-29 23:25:13 +0600

    ghostty tweaks

--- modules/home-manager/terminal/emulators/ghostty.nix
+++ modules/home-manager/terminal/emulators/ghostty.nix
@@ -33,15 +33,15 @@ in
         font-style-italic = "Medium Italic";
         font-style-bold-italic = "Bold Italic";
         window-decoration = false;
-        window-padding-x = 4;
-        window-padding-y = "4,0";
-        window-padding-balance = false;
+        window-padding-x = 8;
+        window-padding-y = "8,0";
+        window-padding-balance = true;
         theme = cfg.theme;
-        background-opacity = 1;
-        background-blur-radius = 20;
+        background-opacity = 0.98;
+        background-blur-radius = 24;
 
         keybind = [
-          "ctrl+backspace=text:\x1b\x7f"
+          "ctrl+backspace=text:\\x1b\\x7f"
           "ctrl+alt+y=jump_to_prompt:-1"
           "ctrl+alt+u=jump_to_prompt:1"
         ];
