c889f46c2dfebe7ef4286222586fa635d1021e39
wasituf <wasit.allin1@proton.me>
2025-07-14 06:38:58 +0600

    initial waybar rice (rose-pine)

--- modules/home-manager/hyprland/waybar/default.nix
+++ modules/home-manager/hyprland/waybar/default.nix
@@ -1,5 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    playerctl
+    wttrbar
+  ];
   programs.waybar = {
     enable = true;
     systemd.enable = true;
@@ -7,20 +11,75 @@
     settings = {
       mainBar = {
         layer = "top";
+        margin = "10px 10px 0 10px";
+        spacing = 6;
         modules-left = [
           "hyprland/workspaces"
-          "hyprland/language"
+          "group/system-info"
+          "privacy"
         ];
-        modules-center = [ "hyprland/window" ];
+        modules-center = [ "mpris" ];
         modules-right = [
-          "clock"
+          "group/stats"
           "tray"
         ];
-        "sway/window" = {
-          max-length = 50;
+        "group/system-info" = {
+          orientation = "horizontal";
+          modules = [
+            "cpu"
+            "memory"
+          ];
+        };
+        cpu = {
+          interval = 2;
+          format = "  {usage}%";
+        };
+        memory = {
+          inverval = 2;
+          format = "  {used:0.1f}G";
+        };
+        privacy = {
+          icon-spacing = 6;
+          icon-size = 16;
+          modules = [
+            {
+              type = "screenshare";
+            }
+            {
+              type = "audio-in";
+            }
+          ];
+        };
+        mpris = {
+          format = "{player_icon} {artist} - {title}";
+          interval = 1;
+          player-icons = {
+            spotify = " ";
+          };
+          title-len = 50;
+          artist-len = 30;
+        };
+        "group/stats" = {
+          orientation = "horizontal";
+          modules = [
+            "custom/weather"
+            "clock"
+          ];
+        };
+        "custom/weather" = {
+          format = "{}°";
+          tooltip = true;
+          interval = 1800;
+          exec = "wttrbar --ampm --location Dhaka";
+          return-type = "json";
         };
         clock = {
-          format-alt = "{:%a, %d. %b  %H:%M}";
+          format = "{:%a %d, %I:%M}";
+          format-alt = "{:%a, %d %b %Y, %I:%M %p}";
+        };
+        tray = {
+          icon-size = 16;
+          spacing = 8;
         };
       };
     };
