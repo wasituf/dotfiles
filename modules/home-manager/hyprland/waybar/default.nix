6956a99c89f8e996e05efe465902eb8db2931394
wasituf <wasit.allin1@proton.me>
2025-07-14 17:09:27 +0600

    waybar update rice

--- modules/home-manager/hyprland/waybar/default.nix
+++ modules/home-manager/hyprland/waybar/default.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, user, ... }:
 {
   home.packages = with pkgs; [
     playerctl
     wttrbar
+    hyprshot
+    satty
+    wl-clipboard
   ];
+  home.file.".config/waybar/nix-logo.png".source = ./nix-logo.png;
   programs.waybar = {
     enable = true;
     systemd.enable = true;
@@ -14,6 +18,7 @@
         margin = "10px 10px 0 10px";
         spacing = 6;
         modules-left = [
+          "group/tools"
           "hyprland/workspaces"
           "group/system-info"
           "privacy"
@@ -23,11 +28,52 @@
           "group/stats"
           "tray"
         ];
+        "group/tools" = {
+          orientation = "horizontal";
+          modules = [
+            "image#os-logo"
+            "group/screenshot"
+          ];
+          drawer = {
+            transition-duration = 200;
+            children-class = "tool-item";
+            click-to-reveal = true;
+          };
+        };
+        "image#os-logo" = {
+          path = "/home/${user}/.config/waybar/nix-logo.png";
+          size = 16;
+          tooltip = false;
+        };
+        "group/screenshot" = {
+          orientation = "horizontal";
+          modules = [
+            "custom/screenshot-output"
+            "custom/screenshot-window"
+            "custom/screenshot-region"
+          ];
+          drawer = {
+            transition-duration = 200;
+          };
+        };
+        "custom/screenshot-output" = {
+          format = "󰹑 ";
+          on-click = ''hyprshot --clipboard-only --raw -m output -m active | satty --copy-command 'wl-copy' --filename - --fullscreen --output-filename ~/screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png'';
+        };
+        "custom/screenshot-window" = {
+          format = " ";
+          on-click = ''hyprshot --clipboard-only --raw -m window | satty --copy-command 'wl-copy' --filename - --fullscreen --output-filename ~/screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png'';
+        };
+        "custom/screenshot-region" = {
+          format = " ";
+          on-click = ''hyprshot --clipboard-only --raw -m region | satty --copy-command 'wl-copy' --filename - --fullscreen --output-filename ~/screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png'';
+        };
         "group/system-info" = {
           orientation = "horizontal";
           modules = [
             "cpu"
             "memory"
+            "gamemode"
           ];
         };
         cpu = {
@@ -37,6 +83,12 @@
         memory = {
           inverval = 2;
           format = "  {used:0.1f}G";
+        };
+        gamemode = {
+          format = "{glyph}";
+          use-icon = false;
+          hide-not-running = false;
+          glyph = "󰓅 ";
         };
         privacy = {
           icon-spacing = 6;
@@ -75,7 +127,8 @@
         };
         clock = {
           format = "{:%a %d, %I:%M}";
-          format-alt = "{:%a, %d %b %Y, %I:%M %p}";
+          format-alt = "{:%a, %d %b %Y - %I:%M %p}";
+          tooltip = false;
         };
         tray = {
           icon-size = 16;
