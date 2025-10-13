64f96ba90f85ebd8453d555833c6e7b9fd36d2b1
wasituf <wasit.allin1@proton.me>
2025-10-14 01:40:23 +0600

    move substituters config to flake.nix

--- flake.nix
+++ flake.nix
@@ -61,11 +61,13 @@
       system-ws = "x86_64-linux";
       tmuxPluginsOverlay = import ./overlays/tmux-plugins.nix;
       substituters = [
+        "https://hyprland.cachix.org"
         "https://aseipp-nix-cache.global.ssl.fastly.net"
         "https://walker.cachix.org"
         "https://walker-git.cachix.org"
       ];
       trusted-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
       ];
--- hosts/ws/default.nix
+++ hosts/ws/default.nix
@@ -53,8 +53,6 @@
       "nix-command"
       "flakes"
     ];
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
   # Garbage collection
