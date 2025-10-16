195f9115bdcd46f15dfcd6ef5691faa33f67bb18
wasituf <wasit.allin1@proton.me>
2025-10-16 15:45:37 +0600

    add to river config

--- modules/home-manager/river/default.nix
+++ modules/home-manager/river/default.nix
@@ -18,12 +18,16 @@ in
       package = null;
       systemd.variables = [ "--all" ];
       settings = {
+        default-layout = "rivertile";
         border-width = 1;
+        border-color-focused = "0xff403d52";
+        border-color-unfocused = "0xff26233a";
         declare-mode = [
           "locked"
           "normal"
           "passthrough"
         ];
+        keyboard-layout = "-variant colemak_dh us";
         input = {
           pointer = {
             accel-profile = "flat";
@@ -35,11 +39,33 @@ in
         map = {
           normal = {
             "Alt Q" = "close";
+            "Super+Shift Q" = "exit";
+            "Super F" = "toggle-fullscreen";
+            "Super Space" = "spawn 'nc -U /run/user/1000/walker/walker.sock'";
             "Alt Return" = "spawn ghostty";
+            "Super Return" = "spawn ghostty";
+            "Super N" = "focus-view next";
+            "Super E" = "focus-view previous";
+            "Super+Shift N" = "swap next";
+            "Super+Shift E" = "swap previous";
+            "Super Period" = "focus-output next";
+            "Super Comma" = "focus-output previous";
+            "Super+Shift Period" = "send-to-output next";
+            "Super+Shift Comma" = "send-to-output previous";
           };
         };
-        # xcursor-theme = "Banana-Catppuccin-Mocha 24";
+        map-pointer = {
+          normal = {
+            "Super BTN_LEFT" = "move-view";
+            "Super BTN_RIGHT" = "resize-view";
+          };
+        };
       };
+      extraConfig = ''
+        rivertile -view-padding 0 -outer-padding 0 &
+        fcitx5 -d -r &
+        fcitx5-remote -r &
+      '';
     };
   };
 }
