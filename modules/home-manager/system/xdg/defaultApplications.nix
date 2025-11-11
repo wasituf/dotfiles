66e1a1d38575072ed49ebe8944d5c18d4954cb72
wasituf <wasit.allin1@proton.me>
2025-11-11 16:45:28 +0600

    refactored and added to default applications config

--- modules/home-manager/system/xdg/defaultApplications.nix
+++ modules/home-manager/system/xdg/defaultApplications.nix
@@ -2,6 +2,19 @@
 with lib;
 let
   cfg = config.modules.system.xdg.defaultApplications;
+  defaultFileManagers = [ "org.gnome.Nautilus.desktop" ];
+  defaultBrowsers = [
+    "zen-beta.desktop"
+    "brave-browser.desktop"
+  ];
+  defaultPdfReaders = [
+    "org.pwmt.zathura.desktop"
+    "zen-beta.desktop"
+  ];
+  defaultImageViewers = [
+    "org.gnome.Loupe.desktop"
+    "feh"
+  ];
 in
 {
   options.modules.system.xdg.defaultApplications = {
@@ -14,13 +27,24 @@ in
       associations.removed = {
       };
       defaultApplications = {
-        "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
-        "text/html" = "zen-beta.desktop";
-        "x-scheme-handler/http" = "zen-beta.desktop";
-        "x-scheme-handler/https" = "zen-beta.desktop";
-        "x-scheme-handler/about" = "zen-beta.desktop";
-        "x-scheme-handler/unknown" = "zen-beta.desktop";
-        "application/pdf" = "org.pwmt.zathura.desktop";
+        "inode/directory" = defaultFileManagers;
+        "application/pdf" = defaultPdfReaders;
+
+        "text/html" = defaultBrowsers;
+        "x-scheme-handler/http" = defaultBrowsers;
+        "x-scheme-handler/https" = defaultBrowsers;
+        "x-scheme-handler/about" = defaultBrowsers;
+        "x-scheme-handler/unknown" = defaultBrowsers;
+
+        "image/svg+xml" = defaultBrowsers;
+        "image/png" = defaultImageViewers;
+        "image/jpeg" = defaultImageViewers;
+        "image/jpg" = defaultImageViewers;
+        "image/gif" = defaultImageViewers;
+        "image/bmp" = defaultImageViewers;
+        "image/tiff" = defaultImageViewers;
+        "image/webp" = defaultImageViewers;
+        "image/avif" = defaultImageViewers;
       };
     };
   };
