47015eb2d328c1cf8cd171265586ecff87af6a53
wasituf <wasit.allin1@proton.me>
2025-11-23 01:40:03 +0600

    update window rules syntax for hyprland

--- modules/home-manager/hyprland/windowrules.nix
+++ modules/home-manager/hyprland/windowrules.nix
@@ -1,14 +1,14 @@
 { ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    windowrulev2 = [
-      "suppressevent maximize, class:.*"
-      "float,class:^(org.gnome.Nautilus)$"
-      "float,class:^(org.gnome.Loupe)$"
-      "float,initialTitle:Zen Browser"
-      "float,class:.*zen-beta.*,title:"
-      "float,class:.*xdg-desktop-portal.*"
-      "pseudo,class:^(fcitx)$"
+    windowrule = [
+      "match:class .*, suppress_event maximize"
+      "match:class ^(org.gnome.Nautilus)$, float yes"
+      "match:class ^(org.gnome.Loupe)$, float yes"
+      "match:initial_title Zen Browser, float yes"
+      "match:class .*zen-beta.*, float yes"
+      "match:class .*xdg-desktop-portal.*, float yes"
+      "match:class ^(fcitx)$, pseudo yes"
     ];
   };
 }
