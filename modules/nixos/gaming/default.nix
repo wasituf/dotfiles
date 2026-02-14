a4d8c5fec3cbcf8c0e798168365878fea90cf627
wasituf <wasit.allin1@proton.me>
2026-02-14 17:22:25 +0600

    add nix-ld support

--- modules/nixos/gaming/default.nix
+++ modules/nixos/gaming/default.nix
@@ -41,10 +41,56 @@ in
 
     environment.systemPackages = with pkgs; [
       protonup-ng
+      (lutris.override {
+        extraLibraries = pkgs: [
+          gamemode
+          mangohud
+        ];
+      })
     ];
 
     environment.sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/${user}/.steam/root/compatibilitytools.d";
+    };
+
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        stdenv.cc.cc
+        openssl
+        curl
+        glib
+        font-misc-misc
+        icu
+
+        glew
+        libGL
+        libGLU
+        libappindicator
+        libvdpau
+        libx11
+        libxext
+        libxfixes
+        libxi
+        libxinerama
+        libxrandr
+        libxrender
+        libxscrnsaver
+        libxxf86vm
+        libsm
+        libice
+        libxtst
+
+        libpulseaudio
+        alsa-lib
+        pipewire
+
+        SDL2
+        SDL2_image
+        SDL2_mixer
+        SDL2_ttf
+        vulkan-loader
+      ];
     };
   };
 }
