7a0c5fdef1ffc9e270be922a308975d41f85d6cc
wasituf <wasit.allin1@proton.me>
2026-03-02 02:28:22 +0600

    add nix-community cache

--- flake.nix
+++ flake.nix
@@ -67,12 +67,14 @@
         ./overlays/tmux-plugins.nix
       ];
       substituters = [
+        "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://aseipp-nix-cache.global.ssl.fastly.net"
         "https://walker.cachix.org"
         "https://walker-git.cachix.org"
       ];
       trusted-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
