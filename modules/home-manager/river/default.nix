8f241d7ea727df560a6e845fd1df88137f1e61f5
wasituf <wasit.allin1@proton.me>
2026-03-02 02:31:34 +0600

    update river config

--- modules/home-manager/river/default.nix
+++ modules/home-manager/river/default.nix
@@ -36,22 +36,59 @@ in
             tap = false;
           };
         };
+        hide-cursor = {
+          timeout = 5000;
+          when-typing = true;
+        };
         map = {
           normal = {
             "Alt Q" = "close";
             "Super+Shift Q" = "exit";
             "Super F" = "toggle-fullscreen";
-            "Super Space" = "spawn 'nc -U /run/user/1000/walker/walker.sock'";
+            "Super S" = "toggle-float";
+            "Super Space" = "spawn \"pkill tofi ; tofi-drun | xargs -I {} riverctl spawn \"{}\"\"";
             "Alt Return" = "spawn ghostty";
             "Super Return" = "spawn ghostty";
-            "Super N" = "focus-view next";
-            "Super E" = "focus-view previous";
-            "Super+Shift N" = "swap next";
-            "Super+Shift E" = "swap previous";
             "Super Period" = "focus-output next";
             "Super Comma" = "focus-output previous";
             "Super+Shift Period" = "send-to-output next";
             "Super+Shift Comma" = "send-to-output previous";
+
+            "Super ]" = "focus-view next";
+            "Super [" = "focus-view previous";
+            "Super M" = "focus-view left";
+            "Super N" = "focus-view down";
+            "Super E" = "focus-view up";
+            "Super I" = "focus-view right";
+
+            "Super+Shift [" = "swap next";
+            "Super+Shift ]" = "swap previous";
+            "Super+Shift M" = "swap left";
+            "Super+Shift N" = "swap down";
+            "Super+Shift E" = "swap up";
+            "Super+Shift I" = "swap right";
+
+            "Alt 1" = "set-focused-tags 1";
+            "Alt 2" = "set-focused-tags 2";
+            "Alt 3" = "set-focused-tags 4";
+            "Alt 4" = "set-focused-tags 8";
+            "Alt 5" = "set-focused-tags 16";
+            "Alt 6" = "set-focused-tags 32";
+            "Alt 7" = "set-focused-tags 64";
+            "Alt 8" = "set-focused-tags 128";
+            "Alt 9" = "set-focused-tags 256";
+            "Alt 0" = "set-focused-tags 512";
+
+            "Alt+Shift 1" = "set-view-tags 1";
+            "Alt+Shift 2" = "set-view-tags 2";
+            "Alt+Shift 3" = "set-view-tags 4";
+            "Alt+Shift 4" = "set-view-tags 8";
+            "Alt+Shift 5" = "set-view-tags 16";
+            "Alt+Shift 6" = "set-view-tags 32";
+            "Alt+Shift 7" = "set-view-tags 64";
+            "Alt+Shift 8" = "set-view-tags 128";
+            "Alt+Shift 9" = "set-view-tags 256";
+            "Alt+Shift 0" = "set-view-tags 512";
           };
         };
         map-pointer = {
